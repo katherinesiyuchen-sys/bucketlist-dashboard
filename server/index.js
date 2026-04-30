260eccf1b97f9d1e911229d604dd27df3436aa90
katherinesiyuchen-sys <katherinesiyuchen@berkeley.edu>
2026-04-30 15:09:55 -0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import badgesRoutes from "./routes/badges.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173", "https://bucketlist-dashboard.vercel.app"] }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
